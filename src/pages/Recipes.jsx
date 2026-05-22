c97ec4d1d759a601f0d3082d6cc0c722c0af2c30
Masanaser <masaNaser@users.noreply.github.com>
2026-05-22 22:04:03 +0300

    add loader

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "react-router-dom";
 export default function Recipes() {
 
   const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchParams] = useSearchParams();
 //   console.log(searchParams);
   const search = searchParams.get('search');
@@ -12,24 +13,44 @@ export default function Recipes() {
 
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const data = await getRecipes(search);
         setRecipes(data);
         console.log(data);
       } catch (error) {
         console.log(error);
       }
+      finally{
+        setIsLoading(false);
+      }
     };
 
     fetchData();
 
   }, [search]);
-
+if(isLoading){
+return (
+      <div className="flex flex-col items-center justify-center min-h-[400px] gap-2">
+        <svg className="animate-spin h-8 w-8 text-amber-950" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <span className="text-amber-950 font-medium">جاري تحميل الوصفات...</span>
+      </div>
+    );
+  }
 return (
   <>
 
     <div className="container mx-auto px-6 py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-
+      {
+        recipes.length === 0?(
+        <div className="text-center text-gray-500 py-10">
+            لم يتم العثور على أي وصفات. حاول البحث بكلمة أخرى!
+          </div>
+        
+     ):
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {recipes.map((recipe) => (
         <div
           key={recipe.id}
@@ -57,6 +78,7 @@ return (
       ))}
 
     </div>
+    }
     </div>
   </>
 );
